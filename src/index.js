4b5d5de2b70e0e1ce4d67ed979a253474aed67ae
KarySorto <88949766+KarySorto@users.noreply.github.com>
2021-10-23 15:29:04 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,72 +1,18 @@
-<!-- Define que el documento esta bajo el estandar de HTML 5 -->
-<!doctype html>
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-<!-- Representa la raíz de un documento HTML o XHTML. Todos los demás elementos deben ser descendientes de este elemento. -->
-<html lang="es">
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-    <head>
-
-<img src="https://cdn.shortpixel.ai/spai/w_788+q_lossless+ret_img+to_auto/https://www.sosfactory.com/wp-content/uploads/2016/12/restaurant-logo-mr-bolat.png" width="200" height="200" align="center">
-        <meta charset="utf-8">
-
-        <title> Formulario de Acceso </title>
-
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="keywords" content="Formulario Acceso, Formulario de LogIn">
-
-        <link href="https://fonts.googleapis.com/css?family=Nunito&display=swap" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css?family=Overpass&display=swap" rel="stylesheet">
-
-        <!-- Link hacia el archivo de estilos css -->
-<link rel="stylesheet" href="css/index.css">
-        <style type="text/css">
-
-        </style>
-
-        <script type="text/javascript">
-
-        </script>
-<script src="https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1"></script>
-<df-messenger
-  intent="WELCOME"
-  chat-title="Restaurante_MrBolat"
-  agent-id="9a664b17-7abb-49b5-a5f8-5e666bea3ab7"
-  language-code="en"
-></df-messenger>
-    </head>
-
-    <body>
-
-        <div id="contenedor">
-            <div id="central">
-                <div id="login">
-                    <div class="titulo">
-                        REGISTRATE
-                    </div>
-                      <form id="loginform">    
-                        <input type="text" name="usuario" placeholder="Usuario" required>
-
- <input type="text" name="Email" placeholder="Correo" required>
-<input type="text" name="Teléfono" placeholder="Telefono" required>
-                        <input type="password" placeholder="Contraseña" name="password" required>
-
-                        <button type="submit" title="Ingresar" name="Ingresar">Ingresar</button>
-                    </form>
-
-                </div>
-
-            </div>
-        </div>
-
-<div id="galeria">
-<img src="https://www.saborusa.com/wp-content/uploads/2019/12/origen-de-la-pizza-1.jpg" width="235" height="200"/>
-<img src="https://www.vidactual.com/rcpmaker/wp-content/uploads/2020/06/PolloPlaza.jpg" width="235" height="200" />
-<img src="https://media-cdn.tripadvisor.com/media/photo-s/18/d0/15/a8/photo1jpg.jpg" width="235" height="200"/>
-<img src="https://cdn.tasteatlas.com/images/dishes/3ab81ec46f604f85bcc3cc51fd17c446.jpg?w=600&h=450" width="235" height="200"/>
-<img src="https://www.xlsemanal.com/wp-content/uploads/sites/3/2018/05/bebidas-768x506.jpg" width="235" height="200"/>
-</div>
-
-
-    </body>
-</html>
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
 
